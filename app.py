1cc33192e314ba4e64696690502dd4deac27350a
Shahid Afridi <74283932+shahidafridi2609@users.noreply.github.com>
2023-12-10 20:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import streamlit as st
 
-st.set_page_config(page_title="Movie Recommendation System", page_icon="Images/animals.gif")
-
 data = pd.read_excel("netflix_data.xlsx")
-
-
-data = load_data()
 
 # Modify 'Title' to ensure it's of string type
 data['Title'] = data['Title'].astype(str)
